a8f876f3778292a22d616e10d7d1d20d354880aa
Mao De Matos <86068971+MaoDeMatos@users.noreply.github.com>
2021-09-15 19:32:01 +0200

    Added projects previews

--- app/views/intro.php
+++ app/views/intro.php
@@ -1,12 +1,12 @@
 <section id="section-style-start" class="color-theme-1"></section>
 
 <section id="section-intro" class="color-theme-2 flex column">
-	<!-- <div class="corner-div-1"></div> -->
+	<div class="corner-div-1"></div>
 
 	<div class="content flex column">
 		<?= $intro_content ?>
 	</div>
-
+<!-- 
 	<svg class="svg-top-left" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
 		<path style="fill: url(#grad-tl)" d="M62.6,-24.5C63.8,-16.5,35.6,-3.1,16,7.7C-3.6,18.6,-14.6,26.9,-18.3,24.4C-22,21.9,-18.5,8.5,-14.2,-3.7C-9.9,-15.9,-5,-26.9,12.9,-31.1C30.7,-35.2,61.4,-32.6,62.6,-24.5Z" transform="translate(60 60)" />
 	</svg>
@@ -14,8 +14,8 @@
 	<svg class="svg-top-right" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
 		<path style="fill: url(#grad-tr)" d="M200,0H0c0,0,53.6,4.3,47.4,89.2c-6.1,83.4,62.2,78.8,72.4,27.8c13-65,72.7-44.4,56.4,37.5
 	c-3.1,15.7,10,14.9,15.6,9.9c8.3-7.4,8.3-15.3,8.3-25.2V0z" />
-	</svg>
+	</svg> -->
 
-	<!-- <div class="corner-div-4 vertical"></div> -->
+	<div class="corner-div-4 vertical"></div>
 
 </section>
